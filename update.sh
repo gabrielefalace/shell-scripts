875fc69f635efe7095ad136e5154877df07baad3
Gabriele Falace <gabrielefalace@gmail.com>
2016-09-28 10:11:58 +0200

    first commit
    updates all the git repos in the current directory (assumes to be executed in the root folder, where each folder is a repository).

---
+++ update.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+RED='0;31'
+GREEN='0;32'
+NC='1;37' # No Color
+
+for d in */ ; do
+    cd "$d"
+    OUTPUT="$(git branch | grep '*')"
+    if [ "$OUTPUT" == "* master" ];
+    then
+        # better check if working directory is actually clean 
+        git pull > /dev/null
+        echo -e "\033[1;32m ✔ \033[0m $d"
+    else
+        echo -e "\033[1;31m ✘ \033[0m $d \033[1;31m (not master) \033[0m"
+    fi
+    cd ..
+done
