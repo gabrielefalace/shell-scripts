ba0f0818306b1ab2d36df25d6b158304f7f2e15b
Gabriele Falace <gabrielefalace@gmail.com>
2016-11-02 10:20:07 +0100

    Update grepping.sh

--- grepping.sh
+++ grepping.sh
@@ -1,1 +1,5 @@
+# grep recursive in current folder (.) only in java files
 grep -rnw --color --include="*.java" . -e "some_word_to_search"
+
+# example: which projects have the selenide dependency?
+grep -rnwl --color --include="*.xml" . -e "selenide" | cut -d "/" -f 2 | uniq
