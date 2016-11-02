c0e95516c301c686f78381fc174b34156923b9eb
Gabriele Falace <gabrielefalace@gmail.com>
2016-11-02 10:41:08 +0100

    Update grepping.sh

--- grepping.sh
+++ grepping.sh
@@ -2,4 +2,5 @@
 grep -rnw --color --include="*.java" . -e "some_word_to_search"
 
 # example: which projects have the selenide dependency?
+# the l switch only shows lines where occurrency is found
 grep -rnwl --color --include="*.xml" . -e "selenide" | cut -d "/" -f 2 | uniq
