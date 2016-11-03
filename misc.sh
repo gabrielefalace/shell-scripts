d7b2c88f6a50131d9a3a2599e464e816d7bcdc4a
Gabriele Falace <gabrielefalace@gmail.com>
2016-11-03 09:33:44 +0100

    Update and rename grepping.sh to misc.sh

--- misc.sh
+++ misc.sh
@@ -4,3 +4,6 @@ grep -rnw --color --include="*.java" . -e "some_word_to_search"
 # example: which projects have the selenide dependency?
 # the l switch only shows lines where occurrency is found
 grep -rnwl --color --include="*.xml" . -e "selenide" | cut -d "/" -f 2 | uniq
+
+# finding all files in current folder (.) with name matching a pattern
+find . -name "*.java"
